2d4ade48da7e06bc3dd767b691b822d9e4de882b
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-20 20:22:48 +0400

    solution way

--- Works/HomeWorks/Problem-Solving-Homworks/js/mad.js
+++ Works/HomeWorks/Problem-Solving-Homworks/js/mad.js
@@ -1,17 +1,17 @@
 function mad(){
-    let dr=document.getElementsByClassName('dr-box')
+    var dr=document.getElementsByClassName('dr-box')
     
 
     let a=0;
     let btn = document.getElementById("btn");
 
-    btn.onclick = function tar(){
+    btn.onclick = function bar(){
         a++;
         console.log(a);
         return a;
     }
     dr[a].style.visibility = "visible";
-
+    
 
 }
 
