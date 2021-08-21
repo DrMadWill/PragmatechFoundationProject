867dfd21c3fce325d0bdb87e6fce6018894194d7
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-21 18:38:18 +0400

    JS variable and data type  research

---
+++ Works/EmptyTime/JS/weekend-work/1-variable/js/will.js
@@ -0,0 +1,25 @@
+// var ,let ,const
+
+// var təyin olunduqdan sonra istənilən yerdə çağırla bilir
+
+
+var b=2;
+console.log(b)
+
+
+// let ancaq özündən sonra çağırlanda təyin olunur
+let a=5;
+
+console.log(a)
+a=9;
+console.log(a)
+
+let ad='star';
+alert(ad);
+
+// const bir dəyərə ancaq birdəfə dəyər atamağa içazə verir
+
+const c=4;
+console.log(c)
+
+c=7;
---
+++ Works/EmptyTime/JS/weekend-work/2-deyiskentipi/js/will.js
@@ -0,0 +1,58 @@
+// deyişken tipler
+
+// typeof deyesenin tipini tapir
+
+// Primitive tip
+
+ // String yazı dəyər
+    let fristname='HauSoku no Haki'
+    console.log(typeof fristname);
+
+ // Number rəqəmlər toplusu dəyəri
+    let age=15;
+    console.log(typeof age)
+    let pul=100.5;
+    console.log(typeof pul)
+
+ // Boolean iki dəyəri olan bir tip seçimlərdə olan dəyər
+    let yang = true;
+    let yin = false;
+    console.log(typeof yang)
+    console.log(typeof yin)
+
+ // null 
+    let para = null
+
+    console.log(typeof para)
+
+
+ // underfined
+    let hayat;
+
+    console.log(typeof hayat)
+     
+//  Reference tip
+
+ // Array
+    let will=['fighter','challenger']
+
+    console.log(typeof will)
+
+ // Object 
+
+  let way = {
+      will:'fighter',
+      way:'learn'
+  }
+
+  console.log(typeof way,'-')
+
+ // Funcition
+    let hesab = function(){
+        return 15
+    }
+
+    console.log(typeof hesab);
+
+
+
