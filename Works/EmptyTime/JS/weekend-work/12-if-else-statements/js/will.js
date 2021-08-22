c856b16c64a0be31c5311eb54c961cbca5c32401
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-23 03:36:58 +0400

    JS If/Else ReSearch

---
+++ Works/EmptyTime/JS/weekend-work/12-if-else-statements/js/will.js
@@ -0,0 +1,25 @@
+const firstname = 'ahmed'
+const age = 22;
+const isStudent = true;
+
+
+
+if (age === 22/*boolean deyer*/){
+    console.log('great')
+}
+
+
+if ((isStudent == false)|| (age >=21 )){
+    console.log('right')
+}else{
+    console.log('merhaa')
+}
+
+
+if ((age>=0)&&(age<=12)){
+    console.log('balaca')
+}else if((age>=13)&&(age<=19)){
+    console.log('tinager')
+}else if((age>=19)&&(age<=25)){
+    console.log('yetiskin')
+}
