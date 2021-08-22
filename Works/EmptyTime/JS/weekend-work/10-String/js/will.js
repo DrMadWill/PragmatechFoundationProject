1edc94458fbbf5733d8a4beb092bf3fc7fd9c7fb
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-22 21:51:59 +0400

    JS String Research

---
+++ Works/EmptyTime/JS/weekend-work/10-String/js/will.js
@@ -0,0 +1,22 @@
+let tar = 'Mad'
+tar +='Will'
+console.log(tar);
+
+// string
+tar=tar.toUpperCase();// boyuk herfle hamsi
+console.log(tar);
+
+tar=tar.toLowerCase();// kicik herf
+console.log(tar);
+
+tar = tar.indexOf('w')// string deyer axtarma
+console.log(tar);
+
+tar = tar.substring(1,7)//yazi secme
+console.log(tar);
+
+tar = tar.slice(1,4)//yazi keib ciarma
+console.log(tar);
+
+tar = tar.replace('Mad','Dr')
+console.log(tar);
