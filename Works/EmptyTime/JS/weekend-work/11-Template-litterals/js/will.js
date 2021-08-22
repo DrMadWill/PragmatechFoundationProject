55f36676ecc6775cd7f20e3b088cb8c62c2e4017
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-22 22:36:24 +0400

    JS Template Litterals Research

---
+++ Works/EmptyTime/JS/weekend-work/11-Template-litterals/js/will.js
@@ -0,0 +1,13 @@
+const fulname = 'MadWill'
+const city = 'StarReaktor'
+const age = 1998;
+
+let a ;
+
+a = `my namre ${fulname} I'am ${2021-age} year old and I live ${city} `
+
+console.log(a);
+
+a = `my namre ${fulname} I'am ${(2021-age>=18)?'over 18':'under 18'}  and I live ${city} `
+
+console.log(a);
