1bebd04bc4d02b08b9a53ca3acc859a005f4aace
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-23 15:26:06 +0400

    Uncomplate >browser centered

---
+++ Works/EmptyTime/JS/weekend-work/14-Loops/js/will.js
@@ -0,0 +1,8 @@
+// Loops
+
+// For Loops
+
+for (let i=0;i<10;++i){
+    console.log(i)
+   
+}
---
+++ Works/HomeWorks/week05-day05/1-homework/js/will.js
@@ -0,0 +1,9 @@
+// document.getElementById("dr-box").style.top = "50%";
+// document.getElementById("dr-box").style.left = "50%";
+// document.getElementById("dr-box").style.transform = "translate(-50%,-50%)";
+
+let a = document.querySelector("#body");
+a= a.offsetWidth
+b= a.offsetHeight
+console.log(a)
+console.log(b)
