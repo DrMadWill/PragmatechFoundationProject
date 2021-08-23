f9257f6a2e7aff55e25ba0857d0e2dedf51bdadd
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-23 15:58:21 +0400

    uncomplate>cetered fix up

--- Works/HomeWorks/week05-day05/1-homework/js/will.js
+++ Works/HomeWorks/week05-day05/1-homework/js/will.js
@@ -2,8 +2,26 @@
 // document.getElementById("dr-box").style.left = "50%";
 // document.getElementById("dr-box").style.transform = "translate(-50%,-50%)";
 
-let a = document.querySelector("#body");
-a= a.offsetWidth
-b= a.offsetHeight
-console.log(a)
-console.log(b)
+
+// let a = document.querySelector("#body");
+// a= a.offsetWidth
+// b= a.offsetHeight
+// console.log(a)
+// console.log(b)
+
+
+
+let d=document.documentElement
+let body = d.body
+let bodygenis=window.innerWidth/2
+let bodyhundur=window.innerHeight/2
+console.log("body-genislik:",bodygenis)
+console.log("body-hundurluk:",bodyhundur)
+
+let div = document.getElementById('dr-box')
+let divgenis=div.clientWidth/2
+let divhundur=div.clientHeight/2
+
+console.log("div-genislik:",divgenis)
+console.log("div-hundurluk:",divhundur)
+
