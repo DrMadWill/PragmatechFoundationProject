8b9d2827df061aaa379356ba2556b52f94ccbc12
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-08-24 14:57:31 +0400

    JS Loops ReSearch

--- Works/EmptyTime/JS/weekend-work/14-Loops/js/will.js
+++ Works/EmptyTime/JS/weekend-work/14-Loops/js/will.js
@@ -2,7 +2,35 @@
 
 // For Loops
 
-for (let i=0;i<10;++i){
-    console.log(i)
-   
-}
+    for (let i=0;i<10;++i){
+        if (i==7){
+            console.log('sevilen--'+i)
+            continue;// hemin dovur sonlandirir novre kecirir
+        }
+        
+        if(i==6){
+            console.log("sevilmeyen------",i)
+            break;// tum donguler sonlanir
+        }
+        console.log(i)
+    
+    }
+
+//   while Loops
+
+    let a = 5;
+    while(a<10){
+        console.log("while coooool loops----",a)
+        a++
+    }
+
+
+//  do whihle 
+
+
+    let c = 15; 
+    do {
+        console.log("do loops haha------",c)
+        c++
+    }while(c<20)
+
