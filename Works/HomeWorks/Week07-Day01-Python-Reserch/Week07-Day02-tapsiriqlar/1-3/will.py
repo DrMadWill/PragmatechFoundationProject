70551a688e68b3bd92252880e0d67250e93fc556
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-05 00:12:28 +0400

    Python > Week07-Day02 > 1-3

---
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/Week07-Day02-tapsiriqlar/1-3/will.py
@@ -0,0 +1,6 @@
+stars="""Proqramalaşdırma nə qədər çox şey bildiyinizlə yox, bildiyinizlə ortaya 
+çıxardığınız işlərlə maraqlanır"""
+
+# str daxilindəki sözləri ayrı bir massiv içərisində toplayın
+m=stars.split(' ')
+print(m)
