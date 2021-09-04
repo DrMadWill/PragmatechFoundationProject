2dd8ae68731bcade4e195a3ead7d31d83b4b7927
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-04 21:20:20 +0400

    Python > Data type conversion

--- Works/HomeWorks/Week07-Day01-Python-Reserch/Data-type-conversion/conversion.py
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/Data-type-conversion/conversion.py
@@ -1,34 +1,34 @@
 # Data type conversion
 
-# a=input()
-# print(type(a))
-# a=int(a)#int type conversion
-# print(type(a))
+a=input()
+print(type(a))
+a=int(a)#int type conversion
+print(type(a))
 
 
 
-# s = 'Dr.MadWill'
-# c = tuple(s)#tuple type conversion
-# print (c)
+s = 'Dr.MadWill'
+c = tuple(s)#tuple type conversion
+print (c)
 
-# d='Gələcəkdə əldə etmək istədiyi bir şey olmayan bir varlıq bu dəyişkən gercəklikdə dəyişkənliyin onun daxilindəki stabiliyi yuyub aparmasina məhkumdur '
+d='Gələcəkdə əldə etmək istədiyi bir şey olmayan bir varlıq bu dəyişkən gercəklikdə dəyişkənliyin onun daxilindəki stabiliyi yuyub aparmasina məhkumdur '
 
-# d=list(d)#list type conversion 
-# print(d)
-
-
+d=list(d)#list type conversion 
+print(d)
 
 
 
 
-# v=['Dəyişkənlikdə','əlində','sabit','olmayan','deklemde','sabit','olan','bizlər','tərfindən','qorxulmağa','məhkumdur']
 
-# d=tuple(v)#tuple type conversion
-# print(d)
 
-# n=True
-# n=int(n)#int type conversion 
-# print(n)
+v=['Dəyişkənlikdə','əlində','sabit','olmayan','deklemde','sabit','olan','bizlər','tərfindən','qorxulmağa','məhkumdur']
+
+l=tuple(v)#tuple type conversion
+print(l)
+
+n=True
+n=int(n)#int type conversion 
+print(n)
 
 
 
