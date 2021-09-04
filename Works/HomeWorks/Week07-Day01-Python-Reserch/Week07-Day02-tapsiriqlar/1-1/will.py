cf1b71d2a9d83327f0b5a26c7e24ace8e891e91a
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-04 21:31:45 +0400

    Python > Week07-Day02 > 1-1

---
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/Week07-Day02-tapsiriqlar/1-1/will.py
@@ -0,0 +1,11 @@
+str='Proqramalaşdırma nə qədər çox şey bildiyinizlə yox, bildiyinizlə ortaya çıxardığınız işlərlə maraqlanır'
+#str daxilində neçə xarakter olduğunu ekrana yazdırın
+
+a=0
+for elmet in str:
+   a+=1 
+print('Daxildə ki, element sayı',end='-')
+print(a)
+
+
+
