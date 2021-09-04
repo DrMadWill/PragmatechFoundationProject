138ef9774ab05f7e18880504db50eb2d7fc63968
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-04 15:42:57 +0400

    Python Data Type

--- Works/HomeWorks/Week07-Day01-Python-Reserch/testing/will.py
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/testing/will.py
@@ -1,34 +1,34 @@
 #String 
-# a='Kralla, kralin oturduğu at arasindakı fərq nədir ? ' #str dəyişən
+a='Kralla, kralin oturduğu at arasindakı fərq nədir ? ' #str dəyişən
 
-# print(type(a))
-# print(a)
+print(type(a))
+print(a)
 
 
-# b="""Sualin cavabi birin at olmasi birin insan olmasi kimi az düşünülmüş cavab deyil 
-# At və kral eyni bacariğa,eyni bədən quruluşuna sahib olsayidi  belə 
-# Yenə biri döyüşü idarə edən kral biri isə kralı daşıyan at olardı.Kralla at arasindakı fərq nədir"""
+b="""Sualin cavabi birin at olmasi birin insan olmasi kimi az düşünülmüş cavab deyil 
+At və kral eyni bacariğa,eyni bədən quruluşuna sahib olsayidi  belə 
+Yenə biri döyüşü idarə edən kral biri isə kralı daşıyan at olardı.Kralla at arasindakı fərq nədir"""
 
-# print(b)
+print(b)
 
-# print(a[0])
+print(a[0])
 
 
 # Number
 
-# print(type(2)) #int
-# print(type(2.5))#float
-# print(type(10/5))#float
-# print(type(2+3j))#complex
+print(type(2)) #int
+print(type(2.5))#float
+print(type(10/5))#float
+print(type(2+3j))#complex
 
 #Sequence 
 
-# d=[1,2,7,'Mad',"Will"] #list
-# print(type(d))
+d=[1,2,7,'Mad',"Will"] #list
+print(type(d))
 
-# e=('Instink',1,3+4)
-# print(type(e))
-# print(e[0:3])
+e=('Instink',1,3+4)
+print(type(e))
+print(e[0:3])
 #e[1]=2;#error assignment
 
 
