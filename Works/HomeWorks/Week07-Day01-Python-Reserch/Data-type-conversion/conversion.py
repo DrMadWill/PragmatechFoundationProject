d3b365de5cb7952ea584efd9109ab4f08514baea
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-04 21:13:55 +0400

    Python Data type conversion

---
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/Data-type-conversion/conversion.py
@@ -0,0 +1,37 @@
+# Data type conversion
+
+# a=input()
+# print(type(a))
+# a=int(a)#int type conversion
+# print(type(a))
+
+
+
+# s = 'Dr.MadWill'
+# c = tuple(s)#tuple type conversion
+# print (c)
+
+# d='Gələcəkdə əldə etmək istədiyi bir şey olmayan bir varlıq bu dəyişkən gercəklikdə dəyişkənliyin onun daxilindəki stabiliyi yuyub aparmasina məhkumdur '
+
+# d=list(d)#list type conversion 
+# print(d)
+
+
+
+
+
+
+# v=['Dəyişkənlikdə','əlində','sabit','olmayan','deklemde','sabit','olan','bizlər','tərfindən','qorxulmağa','məhkumdur']
+
+# d=tuple(v)#tuple type conversion
+# print(d)
+
+# n=True
+# n=int(n)#int type conversion 
+# print(n)
+
+
+
+m=15
+j=bool(m)
+print(j)
