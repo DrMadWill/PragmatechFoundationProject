b1effc3ecce841f142f8dadd735c9512ff38b4a7
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-05 01:03:49 +0400

    Python > Week07-Day02 > 1-6

---
+++ Works/HomeWorks/Week07-Day01-Python-Reserch/Week07-Day02-tapsiriqlar/1-6/will.py
@@ -0,0 +1,18 @@
+strs='Proqramalaşdırma nə qədər çox şey bildiyinizlə yox, bildiyinizlə ortaya çıxardığınız işlərlə maraqlanır'
+
+# str ni vergülə görə ayırıb iki string halına gətirin
+# stringSearch(word) adında bir metod yazın. daxil edilən sözün mətnin içində olub olmadığını ekrana çap edən metod yazın
+
+
+
+d=strs.split(',')
+
+a=d[0]
+b=d[1]
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+
