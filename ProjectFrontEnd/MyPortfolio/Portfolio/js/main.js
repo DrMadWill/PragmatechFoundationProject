4c59c5543019989210e5e48ba61d259b5baebe2e
DrMadWill <nofelsalahov9@gmail.com>
2021-09-13 16:49:21 +0400

    MyProject > header > Js > fix up >different width icon click

--- ProjectFrontEnd/MyPortfolio/Portfolio/js/main.js
+++ ProjectFrontEnd/MyPortfolio/Portfolio/js/main.js
@@ -5,19 +5,46 @@ a= true;
 d=document.getElementById("lost"); 
 c=document.getElementById('lost1');
 
+let m
+m=window.innerWidth
+if (m<768){
+    d.style.display='none'
+    c.style.display='none'
+}
+
+
 function iconclick(){
     
     if (a===true){
         
         a=false;
-        d.style.display='none'
-        c.style.display='none'
-        console.log(a);
-    }else{
-        a=true;
         d.style.display='flex'
         c.style.display='flex'
-        console.log("twoe click")
+        
+    }else{
+        a=true;
+        d.style.display='none'
+        c.style.display='none'
+        
 
     }
+}
+
+
+
+
+function will(){
+    let f
+
+    f=window.innerWidth
+
+    if (f>768){
+        d.style.display='flex'
+        c.style.display='flex'
+    }else{
+        d.style.display='none'
+        c.style.display='none'
+    }
+
+
 }
