5c02cf260a9acfde2f48412f72e6d0cbf3dc20d7
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-22 12:21:54 +0400

    MyPortfolio > Back End add > info -->  Adminpanel --> Database --> MyProject

--- ProjectBackEnd/app.py
+++ ProjectBackEnd/app.py
@@ -7,30 +7,55 @@ db = SQLAlchemy(app)
 
 
 
-class homein(db.Model):
+class Homein(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     info = db.Column(db.String(80), nullable=False)
+    infoimg=db.Column(db.String(200))
     
 
     def __repr__(self):
         return '<User %r>' % self.info
 
 
+class Aboutin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aboutinfo1 = db.Column(db.String(160), nullable=False)
+    aboutinfo2 = db.Column(db.String(160), nullable=False)
+    disgen1 = db.Column(db.String(25))
+    disgen1interest = db.Column(db.String(5))
+    aboutinfoimg=db.Column(db.String(200))
+    
+
+    def __repr__(self):
+        return '<User %r>' % self.aboutinfo1
+
+
+class Projectin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    protime = db.Column(db.String(25), nullable=False)
+    protitle = db.Column(db.String(50), nullable=False)
+    prosortcut = db.Column(db.String(80), nullable=False)
+    aboutinfoimg=db.Column(db.String(200))
+    
+
+    def __repr__(self):
+        return '<User %r>' % self.protitle
 
 # HomeInf=['We make beautiful websites for all people.']
 
 
-infose =[
-    {
-        'cardTime':'December 25.07.21`',
-        'cardTitle':'how to become a web developer',
-        'cardText':'Lorem ipsum dolor sit consectetur adipiscing morbi venenatis.'
-    },
-    {
-        'cardTime':'December 25.07.21`',
-        'cardTitle':'how to become a web developer',
-        'cardText':'Lorem ipsum dolor sit consectetur adipiscing morbi venenatis.'
-    }
 
-]
+# infose =[
+#     {
+#         'cardTime':'December 25.07.21`',
+#         'cardTitle':'how to become a web developer',
+#         'cardText':'Lorem ipsum dolor sit consectetur adipiscing morbi venenatis.'
+#     },
+#     {
+#         'cardTime':'December 25.07.21`',
+#         'cardTitle':'how to become a web developer',
+#         'cardText':'Lorem ipsum dolor sit consectetur adipiscing morbi venenatis.'
+#     }
+
+# ]
 
--- ProjectBackEnd/run.py
+++ ProjectBackEnd/run.py
@@ -2,10 +2,17 @@ from flask import Flask,redirect,render_template,request
 from app import *
 
 
-app=Flask(__name__)
+
 @app.route('/')
 def main():
-    return render_template('maxwill.html',infos=infose)
+    hinfolen=len(Homein.query.all())-1
+    homeinfoall=Homein.query.all()
+    homeinfo=homeinfoall[hinfolen]
+
+    leng=len(Projectin.query.all())-4
+    home = Projectin.query.all()
+    home =home[::-1]
+    return render_template('maxwill.html',home=home,len=leng,homeinfo=homeinfo)
 
 
 @app.route('/project')
@@ -16,15 +23,18 @@ def project():
 @app.route('/admin',methods=['GET','POST'])
 def admin():
     if request.method=='POST':
-        HomeInf =homein(
-            info=request.form.get('HomeTitleText')) ;
-
-        infose[0]['cardTime']=request.form.get('cardTime');
-        infose[0]['cardTitle']=request.form.get('cardTitle');
-        infose[0]['cardText']=request.form.get('cardText')
-        db.session.add(HomeInf)
+        homeinfo=Homein(
+            info=request.form.get('HomeTitleText')
+        )
+        # proinfo=Projectin(
+        #     protime=request.form.get('cardTime'),
+        #     protitle=request.form.get('cardTitle'),
+        #     prosortcut=request.form.get('cardText')
+        # )
+        db.session.add(homeinfo)
         db.session.commit()
         return redirect('/admin')
+    
     return render_template('admin/adminpanel.html')
 
 
