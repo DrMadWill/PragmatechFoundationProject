17a18580059967230dcd07144612a2a9005881f8
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-09-24 17:16:38 +0400

    Myprotfolio > Back end > CRUD ---> Create,Read,Update and Delete complate

--- ProjectBackEnd/postd/routes.py
+++ ProjectBackEnd/postd/routes.py
@@ -150,7 +150,7 @@ def adminpro():
         )
         db.session.add(proinfo)
         db.session.commit()
-        return redirect(adminhome)
+        return redirect(url_for('adminmain'))
     
     return render_template('admin/adminpanelpro.html')
 
@@ -215,11 +215,58 @@ def aboutdelete(id):
 def edithome(id):
     home = Homein.query.get_or_404(id)
     if request.method == 'POST':
-       file = request.files.get('file')
+       file = request.files['file']
+       print(file)
        filename = secure_filename(file.filename)
        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-       home.info = request.form.get('title')
+       home.info = request.form.get('HomeTitleText')
        home.infoimg = filename
        db.session.commit()
        return redirect(url_for("adminmain"))
-    return render_template('admin/edithome.html', home=home)
+    return render_template('admin/edithome.html',homes=home)
+
+
+# -----------------------Admin Panel Project Edit--------------------
+@app.route('/admin/pro/edit/<int:id>', methods=['GET','POST'])
+def editpro(id):
+    proce=Projectin.query.get_or_404(id)
+    if request.method=='POST':
+        file = request.files['pfile']
+        print(file)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        proce.protime=request.form.get('cardTime')
+        proce.protitle=request.form.get('cardTitle')
+        proce.prosortcut=request.form.get('cardText')
+        proce.proimg=filename
+        db.session.commit()
+        return redirect(url_for("adminmain"))
+    
+    return render_template('admin/editpro.html',proce=proce)
+
+
+@app.route('/admin/about/edit/<int:id>', methods=['GET','POST'])
+def editabout(id):
+    aboute=Aboutin.query.get_or_404(id)
+    if request.method == 'POST':
+        file = request.files.get('afile')
+        print(file)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        aboute.aboutinfo1=request.form.get('info1')
+        aboute.aboutinfo2=request.form.get('info2')
+        aboute.disgen1=request.form.get('disgen1')
+        aboute.disgen1interest=request.form.get('disgen1interest')
+        aboute.disgen2=request.form.get('disgen2')
+        aboute.disgen2interest=request.form.get('disgen2interest')
+        aboute.disgen3=request.form.get('disgen3')
+        aboute.disgen3interest=request.form.get('disgen3interest')
+        aboute.disgen4=request.form.get('disgen4')
+        aboute.disgen4interest=request.form.get('disgen4interest')
+        aboute.disgen5=request.form.get('disgen5')
+        aboute.disgen5interest=request.form.get('disgen5interest')
+        aboute.aboutinfoimg=filename
+        db.session.commit()
+        return redirect(url_for("adminmain"))
+    return render_template('admin/editabout.html',aboute=aboute)
+    
